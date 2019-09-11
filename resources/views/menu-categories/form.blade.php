cdbb46a82886d193dfd3ae801cd09c7d29e1b3bd
Daniel (TMP) Seuffer <authanram@gmail.com>
2019-09-12 01:08:05 +0200

    Add Laravel 6 Support

--- resources/views/menu-categories/form.blade.php
+++ resources/views/menu-categories/form.blade.php
@@ -1,13 +1,13 @@
 <div class="form-group {{ $errors->has('name') ? 'has-error' : ''}}">
     <label for="name" class="col-md-4 control-label">{{ 'Name' }}</label>
     <div class="col-md-6">
-        <input class="form-control" name="name" type="text" id="name" value="{{ $menucategory->name or ''}}" required>
+        <input class="form-control" name="name" type="text" id="name" value="{{ $menucategory->name ?? ''}}" required>
         {!! $errors->first('name', '<p class="help-block">:message</p>') !!}
     </div>
 </div><div class="form-group {{ $errors->has('order') ? 'has-error' : ''}}">
     <label for="order" class="col-md-4 control-label">{{ 'Order' }}</label>
     <div class="col-md-6">
-        <input class="form-control" type="number" name="order" type="number" id="order" value="{{ $menucategory->order or ''}}" required>
+        <input class="form-control" type="number" name="order" type="number" id="order" value="{{ $menucategory->order ?? ''}}" required>
         {!! $errors->first('order', '<p class="help-block">:message</p>') !!}
     </div>
 </div><div class="form-group {{ $errors->has('status') ? 'has-error' : ''}}">
@@ -23,6 +23,6 @@
 
 <div class="form-group">
     <div class="col-md-offset-4 col-md-4">
-        <input class="btn btn-primary btn-sm" type="submit" value="{{ $submitButtonText or 'Create' }}">
+        <input class="btn btn-primary btn-sm" type="submit" value="{{ $submitButtonText ?? 'Create' }}">
     </div>
 </div>
--- resources/views/menus/form.blade.php
+++ resources/views/menus/form.blade.php
@@ -4,7 +4,7 @@
         <select class="form-control" name="menu_category_id" id="menu_category_id" required>
             <option>--</option>
             @foreach($menucategories as $category)
-                <option value="{{ $category->id }}"{{ isset($menu) && $menu->menu_category_id === $category->id ? ' selected' : '' }}>
+                <option value="{{ $category->id }}" {{ isset($menu) && $menu->menu_category_id === $category->id ? ' selected' : '' }}>
                     {{ $category->name }}
                 </option>
             @endforeach
@@ -14,25 +14,25 @@
 </div><div class="form-group {{ $errors->has('number') ? 'has-error' : ''}}">
     <label for="number" class="col-md-4 control-label">{{ 'Number' }}</label>
     <div class="col-md-6">
-        <input class="form-control" type="number" name="number" type="number" id="number" value="{{ $menu->number or ''}}" required>
+        <input class="form-control" type="number" name="number" type="number" id="number" value="{{ $menu->number ?? ''}}" required>
         {!! $errors->first('number', '<p class="help-block">:message</p>') !!}
     </div>
 </div><div class="form-group {{ $errors->has('name') ? 'has-error' : ''}}">
     <label for="name" class="col-md-4 control-label">{{ 'Name' }}</label>
     <div class="col-md-6">
-        <input class="form-control" name="name" type="text" id="name" value="{{ $menu->name or ''}}" required>
+        <input class="form-control" name="name" type="text" id="name" value="{{ $menu->name ?? ''}}" required>
         {!! $errors->first('name', '<p class="help-block">:message</p>') !!}
     </div>
 </div><div class="form-group {{ $errors->has('note') ? 'has-error' : ''}}">
     <label for="note" class="col-md-4 control-label">{{ 'Note' }}</label>
     <div class="col-md-6">
-        <input class="form-control" name="note" type="text" id="note" value="{{ $menu->note or ''}}" >
+        <input class="form-control" name="note" type="text" id="note" value="{{ $menu->note ?? ''}}" >
         {!! $errors->first('note', '<p class="help-block">:message</p>') !!}
     </div>
 </div><div class="form-group {{ $errors->has('price') ? 'has-error' : ''}}">
     <label for="price" class="col-md-4 control-label">{{ 'Price' }}</label>
     <div class="col-md-6">
-        <input class="form-control" type="number" step="any" name="price" type="text" id="price" value="{{ $menu->price or ''}}" required>
+        <input class="form-control" type="number" step="any" name="price" type="text" id="price" value="{{ $menu->price ?? ''}}" required>
         {!! $errors->first('price', '<p class="help-block">:message</p>') !!}
     </div>
 </div><div class="form-group {{ $errors->has('status') ? 'has-error' : ''}}">
@@ -48,6 +48,6 @@
 
 <div class="form-group">
     <div class="col-md-offset-4 col-md-4">
-        <input class="btn btn-primary btn-sm" type="submit" value="{{ $submitButtonText or 'Create' }}">
+        <input class="btn btn-primary btn-sm" type="submit" value="{{ $submitButtonText ?? 'Create' }}">
     </div>
 </div>
